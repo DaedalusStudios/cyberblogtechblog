4216bb55a99efe18ecf38c4032e404b3a17add97
= <the.digital.avatar@gmail.com>
2024-02-18 23:31:07 -0700

    file casing is bad on heroku

--- models/index.js
+++ models/index.js
@@ -2,20 +2,21 @@ const Posts = require('./posts.js');
 const Users = require('./users.js');
 const Comments = require('./comments.js');
 
-    Users.hasMany(Posts, {
-      foreignKey: 'id'
-    });
+// Define associations
+Users.hasMany(Posts, {
+  foreignKey: 'user_id' // Foreign key in the Posts table referencing Users table
+});
 
-    Posts.belongsTo(Users, {
-        foreignKey: 'id',
-    });
+Posts.belongsTo(Users, {
+  foreignKey: 'user_id' // Foreign key in the Posts table referencing Users table
+});
 
-    Posts.hasMany(Comments, {
-      foreignKey: 'post_id'
-    });
-    
-    Comments.belongsTo(Posts, {
-      foreignKey: 'id'
-    });
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id' // Foreign key in the Comments table referencing Posts table
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id' // Foreign key in the Comments table referencing Posts table
+});
 
 module.exports = { Users, Posts, Comments };
