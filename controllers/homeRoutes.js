42638cf7e1d8b08319ab75eebf5fcdfd5b5ef88b
= <the.digital.avatar@gmail.com>
2024-02-18 22:52:05 -0700

    file casing is bad on heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,6 +41,7 @@ router.get('/post/:id', async (req, res) => {
       ]
     });
     const post = postData.toJSON();
+    console.log(post);
     res.render('singlePost', {
       post,
       loggedIn: req.session.loggedIn,
